3a6569954fcd30dfe860fb406c857323499ab12c
Maharshi Dindoliwala <maharshi1731@gmail.com>
2026-03-16 10:57:22 -0400

    New Update: Logs

--- waf/app.py
+++ waf/app.py
@@ -1,20 +1,39 @@
 from fastapi import FastAPI, Request
 import requests
 from detector import detect_attack
+from datetime import datetime
 
 app = FastAPI()
 
 BACKEND = "http://backend"
 
-@app.api_route("/{path:path}", methods=["GET","POST","PUT","DELETE"])
+
+def log_attack(ip, attack_type, payload):
+
+    with open("logs/attacks.log", "a") as f:
+        f.write(
+            f"[{datetime.now()}] IP: {ip} | Attack: {attack_type} | Payload: {payload}\n"
+        )
+
+
+@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def proxy(path: str, request: Request):
 
     payload = str(request.url)
 
     attack = detect_attack(payload)
 
+    client_ip = request.client.host
+
     if attack:
-        return {"blocked": attack}
+
+        log_attack(client_ip, attack, payload)
+
+        return {
+            "status": "blocked",
+            "attack_type": attack,
+            "ip": client_ip
+        }
 
     resp = requests.get(f"{BACKEND}/{path}")
 
